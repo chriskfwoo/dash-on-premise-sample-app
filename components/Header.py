7194a519b966cf3586f03f4abe97bd35cc5abb94
Chris Woo <chriskfwoo@gmail.com>
2019-07-17 12:12:17 -0400

    change header color

--- components/Header.py
+++ components/Header.py
@@ -16,7 +16,7 @@ def Header(title, app):
                 children=title,
                 style={
                     'fontSize': 35,
-                    'color': "#108043"
+                    'color': "#50B83C"
                 }
             ),
             Column(
